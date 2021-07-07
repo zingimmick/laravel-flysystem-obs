ae408643f804ddd895041d77dd87a3f704f2b405
zingimmick <zingimmick@outlook.com>
2021-07-07 18:33:56 +0800

    Upgrade Flysystem to 2.0.0

--- src/ObsServiceProvider.php
+++ src/ObsServiceProvider.php
@@ -4,45 +4,35 @@ declare(strict_types=1);
 
 namespace Zing\LaravelFlysystem\Obs;
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 use League\Flysystem\Filesystem;
+use League\Flysystem\Visibility;
 use Obs\ObsClient;
-use Zing\Flysystem\Obs\ObsAdapter;
-use Zing\Flysystem\Obs\Plugins\FileUrl;
-use Zing\Flysystem\Obs\Plugins\Kernel;
-use Zing\Flysystem\Obs\Plugins\SetBucket;
-use Zing\Flysystem\Obs\Plugins\SignUrl;
-use Zing\Flysystem\Obs\Plugins\TemporaryUrl;
+use Zing\Flysystem\Obs\ObsAdapter as LeagueObsAdapter;
+use Zing\Flysystem\Obs\PortableVisibilityConverter;
 
 class ObsServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
         Storage::extend('obs', function ($app, $config) {
-            $root = $config['root'] ?? null;
-            $options = array_merge(
-                Arr::only($config, ['url', 'temporary_url', 'bucket_endpoint']),
-                $config['options'] ?? []
+            $root = $config['root'] ?? '';
+            $options = $config['options'] ?? [];
+            $portableVisibilityConverter = new PortableVisibilityConverter(
+                $config['visibility'] ?? Visibility::PUBLIC
             );
-            $obsAdapter = new ObsAdapter(
-                new ObsClient($config),
-                $config['endpoint'],
+            $obsClient = new ObsClient($config);
+            $obsAdapter = new LeagueObsAdapter(
+                $obsClient,
                 $config['bucket'],
                 $root,
+                $portableVisibilityConverter,
+                null,
                 $options
             );
 
-            $filesystem = new Filesystem($obsAdapter, $config);
-
-            $filesystem->addPlugin(new FileUrl());
-            $filesystem->addPlugin(new SignUrl());
-            $filesystem->addPlugin(new TemporaryUrl());
-            $filesystem->addPlugin(new SetBucket());
-            $filesystem->addPlugin(new Kernel());
-
-            return $filesystem;
+            return new ObsAdapter(new Filesystem($obsAdapter, $config), $obsAdapter, $config, $obsClient);
         });
     }
 }
--- tests/DriverTest.php
+++ tests/DriverTest.php
@@ -6,13 +6,12 @@ namespace Zing\LaravelFlysystem\Obs\Tests;
 
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Storage;
-use Zing\Flysystem\Obs\ObsAdapter;
 
 class DriverTest extends TestCase
 {
     public function testDriverRegistered(): void
     {
-        self::assertInstanceOf(ObsAdapter::class, Storage::disk('obs')->getDriver()->getAdapter());
+        self::assertInstanceOf(\Zing\LaravelFlysystem\Obs\ObsAdapter::class, Storage::disk('obs'));
     }
 
     public function testUrl(): void
