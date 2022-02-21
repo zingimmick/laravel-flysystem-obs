5262be110e47425d85e4451e40dd2e44c2080c99
zingimmick <zingimmick@outlook.com>
2022-02-21 18:46:01 +0800

    Bump zing/coding-standard from ^5.2 to ^6.1

--- ecs.php
+++ ecs.php
@@ -2,9 +2,6 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\ControlStructure\YodaStyleFixer;
-use PhpCsFixer\Fixer\PhpUnit\PhpUnitInternalClassFixer;
-use PhpCsFixer\Fixer\PhpUnit\PhpUnitTestClassRequiresCoversFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 use Zing\CodingStandard\Set\ECSSetList;
@@ -14,11 +11,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(ECSSetList::CUSTOM);
 
     $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::SKIP, [
-        YodaStyleFixer::class => null,
-        PhpUnitInternalClassFixer::class,
-        PhpUnitTestClassRequiresCoversFixer::class,
-    ]);
+    $parameters->set(Option::PARALLEL, true);
     $parameters->set(
         Option::PATHS,
         [__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']
