56c03d6c2070a138f8adcc47271611adba477f37
zingimmick <zingimmick@outlook.com>
2022-09-15 22:55:57 +0800

    Bump illuminate/filesystem to ^9.30

--- tests/DriverTest.php
+++ tests/DriverTest.php
@@ -38,17 +38,8 @@ final class DriverTest extends TestCase
         self::assertStringStartsWith('https://your-endpoint', Storage::disk('obs-bucket-endpoint')->url('test'));
     }
 
-    private function supportIsCname(): bool
-    {
-        return version_compare(ObsClient::SDK_VERSION, '3.21.6', '>=');
-    }
-
     public function testIsCname(): void
     {
-        if (! $this->supportIsCname()) {
-            self::markTestSkipped('Option `is_cname` not supported.');
-        }
-
         self::assertStringStartsWith(
             'https://your-endpoint',
             Storage::disk('obs-bucket-endpoint')->temporaryUrl('test', Carbon::now()->addMinutes())
