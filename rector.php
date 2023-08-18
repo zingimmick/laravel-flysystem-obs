ffda363f651cf403ba5c01896368bfe9478857eb
Zing <zingimmick@outlook.com>
2023-08-19 05:29:10 +0800

    Update rector.php

--- rector.php
+++ rector.php
@@ -20,7 +20,6 @@ return static function (RectorConfig $rectorConfig): void {
         RenameParamToMatchTypeRector::class,
         AddSeeTestAnnotationRector::class,
         FinalizeClassesWithoutChildrenRector::class,
-        RepeatedLiteralToClassConstantRector::class,
     ]);
     $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']);
 };
